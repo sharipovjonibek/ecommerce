958e0ef04620eb050f1cf093078aaacc9ef0d9f7
Sharipov Jonibek <jsharipov559@gmail.com>
2026-01-08 15:58:04 +0500

    work on 'Profile management'

--- account/urls.py
+++ account/urls.py
@@ -20,7 +20,13 @@ urlpatterns=[
 
     path('user-logout',views.user_logout,name='user-logout'),
 
+    # Dashboard / profile urls
+
     path('dashboard',views.dashboard,name='dashboard'),
+
+    path('profile-management',views.profile_management,name='profile-management'),
+
+    path('delete-account',views.delete_account,name='delete-account'),
     
     
 ]
