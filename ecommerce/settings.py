3b8056a9f31e7bbd418a4494fd506e72b6a2d15b
Sharipov Jonibek <jsharipov559@gmail.com>
2026-02-13 23:56:37 +0500

    Update ALLOWED_HOST

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "magazincha.uz",
     "www.magazincha.uz",
-    '*'
+    ".elasticbeanstalk.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = ['https://magazincha.uz','https://www.magazincha.uz']
